357ebc8e97d3377f2b88a5db97fb4cca3205eeca
renehorstmann <r.horsty@gmail.com>
2021-03-12 11:44:48 +0100

    prandom

--- include/u/prandom.h
+++ include/u/prandom.h
@@ -12,6 +12,11 @@ void u_pseed(uint32_t seed);
 // [0 : uPRAND_MAX)
 uint32_t u_prand();
 
+
+#ifdef MAHTC_UTILS_RANDOM
+#error include prandom.h before mathc/utils/random.h (mathc/mathc.h, mathc/utils/float.h)!
+#endif
+
 #define MATHC_RANDOM_FUNCTION u_prand
 #define MATHC_RANDOM_FUNCTION_MAX uPRAND_MAX
 #include "mathc/utils/random.h"
