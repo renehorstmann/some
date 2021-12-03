4e9c29b2d805de84b649ba8b8dd789010de3978b
renehorstmann <r.horsty@gmail.com>
2021-12-03 09:55:55 +0100

    test

--- src/main.c
+++ src/main.c
@@ -83,7 +83,7 @@ int main(int argc, char **argv) {
     ////
 
 //    L.fetch = u_fetch_new_get("https://rohl.svenhuis.de/api/test");
-    L.fetch = u_fetch_new_post("https://rohl.svenhuis.de/api/test", strc("entry=10166~Waluigi4~39672"));
+    L.fetch = u_fetch_new_post("https://rohl.svenhuis.de/api/test", strc("entry=10200~Waluigi4~39672"));
 
     // start the main loop, blocking call
     e_window_main_loop(L.window, main_loop);
