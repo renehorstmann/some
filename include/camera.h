39c0d895792575065f88e64861cd35cdd7f2d50b
renehorstmann <r.horsty@gmail.com>
2022-01-24 17:54:01 +0100

    gpush

--- include/camera.h
+++ include/camera.h
@@ -10,26 +10,48 @@
 #include "mathc/types/float.h"
 #include "mathc/types/int.h"
 
-
+// the camera will unsure a canvas of at least CAMERA_SIZE * CAMERA_SIZE units
 #define CAMERA_SIZE 180 // *4=720; *6=1080; *8=1440
 
+
 typedef struct {
-    mat4 v;         // view / pose of the camera in 3d space
+    // view / pose of the camera in 3d space
+    mat4 v;         
     mat4 v_inv;     // inv(v)
-    mat4 p;         // projection of the camera (perspective / orthogonal)
+    
+    // projection of the camera (perspective / orthogonal)
+    // pass 'p' to a render object, 
+    //     to draw it at a static position on the screen (HUD)
+    // use 'p_inv' to transform a touch position to the screen units
+    mat4 p;         
     mat4 p_inv;     // inv(p)
-    mat4 vp;        // p @ v_inv   used for render objects (ro)
-    mat4 v_p_inv;   // v @ p_inv   used for input
+    
+    // combination of view and projection
+    // pass 'vp' to a render object,
+    //     to draw it in the world, 
+    //     so that the camera movement of 'v' is used
+    // use 'v_p_inv' to transform a touch position to the world
+    mat4 vp;        // p @ v_inv
+    mat4 v_p_inv;   // v @ p_inv
 } CameraMatrices_s;
 
 typedef struct {
     CameraMatrices_s matrices;
 
     struct {
-        float scale;    // units per pixel
-        float left, right, bottom, top;
+         // units per pixel
+        float scale;
+        
+        // static camera borders in units
+        // bottom and right may be a little behind the actual screen border (depending on the real resolution)
+        int left, right, bottom, top;
+        
+        // static camera borders in units
+        // bottom and right are on the actual screen borders
+        float screen_left, screen_right, screen_bottom, screen_top;
     } RO;   // read only
 } Camera_s;
+
 
 // creates a new camera
 Camera_s camera_new();
@@ -40,19 +62,19 @@ void camera_update(Camera_s *self, ivec2 window_size);
 // set the position of the camera in the world
 void camera_set_pos(Camera_s *self, float x, float y);
 
-// set the size / zoom of the camera
+// set the size / zoom of the camera (default = 1.0)
 void camera_set_size(Camera_s *self, float size);
 
 // set the rotation of the camera
 void camera_set_angle(Camera_s *self, float alpha);
 
 // returns the width in game pixel (!=real pixel)
-static float camera_width(const Camera_s *self) {
+static int camera_width(const Camera_s *self) {
     return -self->RO.left + self->RO.right;
 }
 
 // returns the height in game pixel (!=real pixel)
-static float camera_height(const Camera_s *self) {
+static int camera_height(const Camera_s *self) {
     return -self->RO.bottom + self->RO.top;
 }
 
